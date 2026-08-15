fbfd3e995cc3f3cb2e1a00193bd0ecda3ad308d4
NaderiEnglish <mohammadreza.naderi888@gmail.com>
2026-08-15 10:26:13 +0330

    Update Base.tsx

--- src/templates/Base.tsx
+++ src/templates/Base.tsx
@@ -1,3 +1,7 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
 import { Meta } from '../layout/Meta';
 import { AppConfig } from '../utils/AppConfig';
 import { Banner } from './Banner';
@@ -5,16 +9,45 @@ import { Footer } from './Footer';
 import { Hero } from './Hero';
 import { Sponsors } from './Sponsors';
 import { VerticalFeatures } from './VerticalFeatures';
+import { LanguageSelector } from '../components/LanguageSelector';
 
-const Base = () => (
-  <div className="text-gray-600 antialiased">
-    <Meta title={AppConfig.title} description={AppConfig.description} />
-    <Hero />
-    <Sponsors />
-    <VerticalFeatures />
-    <Banner />
-    <Footer />
-  </div>
-);
+type Language = 'en' | 'fa';
+
+const Base = () => {
+  const [language, setLanguage] = useState<Language>('en');
+
+  useEffect(() => {
+    const savedLanguage = localStorage.getItem('language') as Language | null;
+
+    if (savedLanguage === 'en' || savedLanguage === 'fa') {
+      setLanguage(savedLanguage);
+    }
+  }, []);
+
+  useEffect(() => {
+    document.documentElement.dir = language === 'fa' ? 'rtl' : 'ltr';
+    document.documentElement.lang = language;
+  }, [language]);
+
+  return (
+    <div
+      className="text-gray-600 antialiased"
+      dir={language === 'fa' ? 'rtl' : 'ltr'}
+    >
+      <Meta
+        title={AppConfig.title}
+        description={AppConfig.description}
+      />
+
+      <LanguageSelector onSelect={setLanguage} />
+
+      <Hero />
+      <Sponsors />
+      <VerticalFeatures />
+      <Banner />
+      <Footer />
+    </div>
+  );
+};
 
 export { Base };
