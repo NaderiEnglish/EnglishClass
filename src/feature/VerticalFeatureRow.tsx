c8703ab8e8b828c0ec0f4471c2e45738d7481289
NaderiEnglish <mohammadreza.naderi888@gmail.com>
2026-08-15 19:21:01 +0330

    Update VerticalFeatureRow.tsx

--- src/feature/VerticalFeatureRow.tsx
+++ src/feature/VerticalFeatureRow.tsx
@@ -25,8 +25,13 @@ const VerticalFeatureRow = (props: IVerticalFeatureRowProps) => {
   return (
     <div className={verticalFeatureClass}>
       <div className="w-full text-center sm:w-1/2 sm:px-6">
-        <h3 className="text-3xl font-semibold text-gray-900">{props.title}</h3>
-        <div className="mt-6 text-xl leading-9">{props.description}</div>
+        <h3 className="text-3xl font-semibold text-gray-900 dark:text-white">
+          {props.title}
+        </h3>
+
+        <div className="mt-6 text-xl leading-9 text-gray-600 dark:text-gray-300">
+          {props.description}
+        </div>
       </div>
 
       <div className="w-full p-6 sm:w-1/2">
